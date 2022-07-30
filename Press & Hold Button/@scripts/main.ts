c10d88361f5028d4e33f7f1572d91b804b1b1b17
jxmked <jamc015lol@gmail.com>
2022-07-30 16:30:30 +0800

    Update

---
+++ Press & Hold Button/@scripts/main.ts
@@ -0,0 +1,75 @@
+
+/**
+ * For Demo - Javascript press and hold Button 
+ * */
+function Start(data : {[id:string]:any}){
+    // Click Every 100ms
+    data["ival"] = setInterval(() => {
+        data["func"]();
+    }, 100)
+}
+
+function Stop(data:{[id:string]:any}){
+    clearInterval(data["ival"]);
+}
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    // Main Input
+    const input : HTMLInputElement = document.querySelector("#num")!;
+    
+    // Init - Increase
+    const i_btn = HoldButton(document.querySelector("#i_num")!);
+    
+    // Main function
+    i_btn.onClick(() => {
+        input.value = String(Number(input.value) + 1);
+    });
+    
+    // Set what will happen if we hold the button
+    i_btn.onHold(Start, 500);
+    
+    // Set what will happen if we unhold the button
+    i_btn.onUnHold(Stop);
+    
+    
+    // Init - Decrease
+    const d_btn = HoldButton(document.querySelector("#d_num")!);
+    
+    // our main function to call
+    d_btn.onClick(() => {
+        input.value = String(Number(input.value) - 1);
+    })
+    
+    // Set what will happen if we hold the button
+    
+    d_btn.onHold(Start, 500);
+    
+    // Set what will happen if we unhold the button
+    d_btn.onUnHold(Stop);
+    
+    
+    /** With Modal // Pop-up action **/
+    
+    // Modal Container
+    const m_modal : HTMLElement = document.querySelector("#hold_modal")!;
+    
+    // Close on click
+    document.querySelector("#hold_modal_close")!.addEventListener("click", () => {
+        m_modal.classList.add("hidden");
+    });
+    
+    // Show Modal after press and hold of main button
+    const m_hb = HoldButton(document.querySelector("#hold_button")!)
+    
+    m_hb.onHold(() => {
+        m_modal.classList.remove("hidden");
+    });
+    
+});
+
+/**
+ * Written By Jovan De Guia
+ * Project Name: Press and Hold For Web Buttons
+ * Github: jxmked
+ * */
---
+++ Press & Hold Button/README.md
@@ -0,0 +1,19 @@
+# Javascript-Press-and-Hold-Feature
+Javascript Press and Hold Feature For Web UI
+
+With this JavaScript function, we can implement press and hold feature in Web UIs.
+
+> Compatible with Mobile and Desktop Devices
+
+[Live Demo](https://jxmked.github.io/Javascript-Press-and-Hold-Feature/)
+----
+
+****Enjoy****
+#### Written by Jovan De Guia
+
+## Socials
+
+- [Github](https://github.com/jxmked)
+- [Facebook](https://www.facebook.com/deguia25)
+
+### License under MIT license
